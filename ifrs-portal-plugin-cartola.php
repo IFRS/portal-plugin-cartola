959b39a2df223b8b5e6b5f1dc1e184f138ec015e
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-05-15 17:10:11 -0300

    Commit inicial

---
+++ ifrs-portal-plugin-cartola.php
@@ -0,0 +1,17 @@
+<?php
+defined('ABSPATH') or die('No script kiddies please!');
+/*
+Plugin Name: IFRS Portal Plugin Cartola
+Plugin URI:  https://github.com/IFRS/portal-plugin-cartola
+Description: Plugin para definir seleção única das categorias de um post (cartola).
+Version:     1.0.0
+Author:      Ricardo Moro
+Author URI:  https://github.com/ricardomoro
+License:     GPL3
+License URI: https://www.gnu.org/licenses/gpl-3.0.txt
+Text Domain: ifrs-portal-plugin-cartola
+Domain Path: /lang
+*/
+
+require_once('taxonomy-single-term/class.taxonomy-single-term.php');
+require_once('cartola.php');
